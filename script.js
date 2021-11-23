7f59c4d15b46d04c6c5033350fca4153c6a847fb
Narhan523 <91747098+Narhan523@users.noreply.github.com>
2021-11-23 16:08:53 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+ScrollReveal().reveal('#zuun', {
+    origin: 'left',
+    distance: '250px'
+})
+
+ScrollReveal().reveal('#baruun', {
+    origin: 'right',
+    distance: '250px'
+})
+
+ScrollReveal().reveal('.hero', {
+    origin: 'left',
+    distance: '100%',
+    duration: 1500
+})
+
+ScrollReveal().reveal('#overlay', {
+    origin: 'left',
+    distance: '100%',
+    duration: 1200,
+    delay: 700
+})
